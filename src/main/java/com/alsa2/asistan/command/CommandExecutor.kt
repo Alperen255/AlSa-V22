f260c31bba116899c37c3ebb196b1f02046c6f68
Alperen255 <alperen.saracoglu@hotmail.com>
2026-03-08 22:58:31 +0300

    Create CommandExecutor.kt file with CommandExecutor class and methods

---
+++ src/main/java/com/alsa2/asistan/command/CommandExecutor.kt
@@ -0,0 +1,23 @@
+package com.alsa2.asistan.command;
+
+public class CommandExecutor {
+    public void click() {
+        // Implementation for click action
+    }
+
+    public void writeText(String text) {
+        // Implementation for writing text
+    }
+
+    public void openApp(String appName) {
+        // Implementation for opening an application
+    }
+
+    public void pressBack() {
+        // Implementation for pressing back
+    }
+
+    public void pressHome() {
+        // Implementation for pressing home
+    }
+}
