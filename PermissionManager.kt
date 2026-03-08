a0bb8de5c3c97c248b54b267391e206f6f5b2740
Alperen255 <alperen.saracoglu@hotmail.com>
2026-03-08 22:56:08 +0300

    Create PermissionManager.kt file with accessibility and overlay permission checking functions

---
+++ PermissionManager.kt
@@ -0,0 +1,13 @@
+package com.alsa2.asistan.utils
+
+object PermissionManager {
+    fun checkAccessibilityServicePermission(): Boolean {
+        // Implementation to check if the accessibility service is enabled.
+        return false // Placeholder return statement
+    }
+
+    fun checkOverlayPermission(): Boolean {
+        // Implementation to check if the overlay permission is granted.
+        return false // Placeholder return statement
+    }
+}
